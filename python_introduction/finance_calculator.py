475ec6cb2bd636a97093ca351f3cadddf946ea4c
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-05-24 20:23:54 +0000

    trying the f-print with the output line of the code

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -34,8 +34,8 @@ Projected savings after one year, with interest, is: $12600.
 SIMPLE_ANNUAL_INTEREST_RATE = 0.05
 
 #take user inputs
-monthly_income = float(input("Enter your monthly income: "))
-monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 #calculate monthly savings
 monthly_savings = monthly_income - monthly_expenses
@@ -44,5 +44,9 @@ monthly_savings = monthly_income - monthly_expenses
 projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * SIMPLE_ANNUAL_INTEREST_RATE)
 
 #displaying results
+"""
 print("Your monthly savings are $", monthly_savings, ".")
 print("Projected savings after one year, with interest, is: $", projected_annual_savings, ".")
+"""
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings}.")
