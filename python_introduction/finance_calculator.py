c4c32787a01c3185df78eaba5285ea5c01d530bc
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-05-24 20:13:57 +0000

    Added finance_calculator.py file

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,48 @@
+"""
+Objective: Use user input, variables, and arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.
+
+Task Description:
+
+You will create a script named finance_calculator.py. This script will calculate the user’s monthly savings based on inputted monthly income and expenses. It will then project these savings over a year, assuming a fixed interest rate, to demonstrate compound interest’s effect on savings.
+
+Instructions:
+
+User Input for Financial Details:
+
+Prompt the user for their monthly income: “Enter your monthly income: ”.
+Ask for their total monthly expenses: “Enter your total monthly expenses: ”.
+Calculate Monthly Savings:
+
+Calculate the monthly savings by subtracting monthly expenses from the monthly income.
+Project Annual Savings:
+
+Assume a simple annual interest rate of 5%.
+Calculate the projected savings after one year, incorporating the interest. Use the simplified formula for annual savings projection: (Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)).
+Output Results:
+
+Display the user’s monthly savings.
+Display the projected annual savings after including interest.
+Example Interaction:
+
+Enter your monthly income: 5000
+Enter your total monthly expenses: 4000
+Your monthly savings are $1000.
+Projected savings after one year, with interest, is: $12600.
+
+"""
+#declaring interest rate constant
+SIMPLE_ANNUAL_INTEREST_RATE = 0.05
+
+#take user inputs
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+#calculate monthly savings
+monthly_savings = monthly_income - monthly_expenses
+
+#calculate projected annual savings
+projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * SIMPLE_ANNUAL_INTEREST_RATE)
+
+#displaying results
+print("Your monthly savings are $", monthly_savings, ".")
+print("Projected savings after one year, with interest, is: $", projected_annual_savings, ".")
