a3bd231a4f557b56def2424f1952e24d94be5b58
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-05-24 19:07:00 +0000

    Added staged basic_operations.py file. Getting ready to push it to GitHub

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#vairable assignments
+number1 = 10
+number2 = 5
+
+#perfoming calculations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#printing results of calculations
+print("Addition of", number1, "and" , number2, "is" , sum)
+print("Subtraction of", number1, "and" , number2, "is" , difference)
+print("Multiplication of", number1, "and" , number2, "is" , product)
