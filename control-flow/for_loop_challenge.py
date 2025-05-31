ddcdaecf01413d5b10606023c367f6d785b81c3e
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-05-31 08:57:54 +0000

    Updated for_loop_challenge.py file. Works as expected! Yaaaay!

--- control-flow/for_loop_challenge.py
+++ control-flow/for_loop_challenge.py
@@ -9,3 +9,14 @@ Inside the loop, add the current number (use the loop variable) to the total var
 After the loop, print the final total value, which represents the sum of all the numbers in the list.
 
 """
+#initializing list of numbers
+numbers = [1, 5, 3, 9]
+total = 0
+
+#for loop to perform the running sum
+for n in numbers:
+    total += n
+    print(total)
+
+#printing final total value
+print(total)
